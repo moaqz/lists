5bf55f5d5bdc1773fc5e658d77769f4298495817
moaqz <goz.matu@gmail.com>
2024-03-18 18:18:47 -0300

    feat: add Laravel Breeze API endpoints

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisteredUserController extends Controller
+{
+    public function store(RegisterRequest $request): Response
+    {
+        $request->validated();
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+        Auth::login($user);
+
+        return response()->noContent();
+    }
+}
