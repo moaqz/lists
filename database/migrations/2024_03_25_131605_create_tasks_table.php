d03fa1d673ebb1c88d04451e0da86da14c65d38a
moaqz <goz.matu@gmail.com>
2024-03-25 20:39:54 -0300

    chore: add `TaskFactory` for database seeding

---
+++ database/migrations/2024_03_25_131605_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->text('content');
+            $table->tinyInteger('priority')->unsigned()->default(0);
+            $table->boolean('completed')->default(false);
+            $table->foreignUuid('group_id')->index();
+            $table->foreignId('user_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\Group;
+use App\Models\Task;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        $group = Group::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        Task::factory(40)->create([
+            'user_id' => $user->id,
+            'group_id' => $group->id,
         ]);
     }
 }
